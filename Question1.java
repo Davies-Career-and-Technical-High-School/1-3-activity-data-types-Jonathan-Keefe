f57e13b3e5686b7ecb604b9faeb3e1441f83861b
Jonathan Keefe <jonathan.keefe@daviestech.org>
2020-10-06 16:46:41 +0000

    Question 1

--- Question1.java
+++ Question1.java
@@ -1,9 +1,17 @@
-
+import java.util.Scanner;
 
 class Question1 {
     public static void main(String[] args) {
 
         /* Write your code here */
+      Scanner scan = new Scanner(System.in);
+      String num1;
+      String num2;
+      System.out.println("Give me a number");
+      num1 = scan.nextLine();
+      System.out.println("Give me another number");
+      num2 = scan.nextLine();
 
+      System.out.println( num2 + " " + num1);
     }
 }
